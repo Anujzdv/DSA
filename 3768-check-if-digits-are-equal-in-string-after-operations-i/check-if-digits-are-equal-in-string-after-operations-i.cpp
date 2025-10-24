83fedb2e3c724d84656284de2630d059c2a6d932
ANUJ KUMAR <anujzdv@gmail.com>
2025-10-24 20:09:45 +0530

    Time: 1 ms (90.23%) | Memory: 9.3 MB (77.98%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int n = s.size();
+        vector<int> digits(n);
+        for (int i = 0; i < n; ++i) {
+            digits[i] = s[i] - '0';
+        }
+
+        while (n > 2) {
+            for (int i = 0; i < n - 1; ++i) {
+                digits[i] = (digits[i] + digits[i + 1]) % 10;
+            }
+            --n;
+        }
+
+        return digits[0] == digits[1];
+    }
+};
